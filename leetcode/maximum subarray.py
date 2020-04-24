c4b3480292ea375291009fe95522e9b6551c2d0f
Chengwen Luo <lcwdaisy@gmail.com>
2020-04-24 23:17:28 +0800

    Update maximum subarray.py

--- leetcode/maximum subarray.py
+++ leetcode/maximum subarray.py
@@ -19,20 +19,15 @@ If you have figured out the O(n) solution, try coding another solution using the
 class Solution:
     def maxSubArray(self, nums) -> int:
         maxsum = nums[0]
-        print(maxsum)
         start_pos = 0
+        single_sum = 0
         for i in range(len(nums)):
-            single_sum = 0
             for j in range(start_pos, i + 1):
                 single_sum += nums[j]
-                print(nums[j],single_sum )
             maxsum = max(maxsum, single_sum)
             if single_sum < 0:
                 start_pos = j + 1
                 single_sum = 0
-
-            print(single_sum, maxsum)
-
         return maxsum
 
 
